bc9daf2749e3232293ee43f0e7787770097f90a4
Alex Cândido <alex.candido.tec@gmail.com>
2022-09-23 20:16:55 -0300

    estrutura Form de Welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import { AiFillPlayCircle } from "react-icons/ai";
-// import { SiEthereum } from "react-icons/si";
-// import { BsInfoCircle } from "react-icons/bs";
+import { BsInfoCircle } from "react-icons/bs";
+import { SiEthereum } from "react-icons/si";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -9,7 +9,7 @@ const Welcome: React.FC  = () => {
   function connectWallet() {
     
   }
-  
+
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -44,6 +44,27 @@ const Welcome: React.FC  = () => {
             <div className={companyCommonStyles}>Low Fees</div>
             <div className={`rounded-br-2xl ${companyCommonStyles}`}>
               Blockchain
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  {shortenAddress(currentAccount)}
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
             </div>
           </div>
         </div>
